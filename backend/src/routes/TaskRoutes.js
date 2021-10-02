b298cd0cfcc0561c0a38b6e6aa1a72f2f49e4feb
Weslley Nascimento <weslleynassjn@gmail.com>
2021-10-02 04:21:24 -0300

    adicionando endpoint de getById

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskContoller');
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
-router.get('/filter/all', TaskController.all);
+router.get('/filter/all', MacaddressValidation, TaskController.all);
+router.get('/:id', TaskController.show);
 
 module.exports = router;
