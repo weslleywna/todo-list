405d9042ba62e92550a382507eef894951abcac3
Weslley Nascimento <weslleynassjn@gmail.com>
2022-04-09 16:45:39 -0300

    component Footer

---
+++ frontend/web/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Footer() {
+    return (
+      <S.Container>
+        <span>ToDo - Organizando Sua Vida</span>
+      </S.Container>
+    );
+  }
+  
+  export default Footer;
+  
--- frontend/web/src/views/Home/index.js
+++ frontend/web/src/views/Home/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import * as S from './styles';
 
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
     <S.Container>
       <Header />
+      <Footer />
     </S.Container>
   );
 }
