b6119bd09ee035e591f100007aeb5fbf28c88f3f
Weslley Nascimento <weslley_sjn@hotmail.com>
2023-07-22 17:12:29 -0300

    criando projeto mobile com react native

---
+++ frontend/todo-mobile/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/views/Home';
+
+export default function App() {
+  return (
+    <Home></Home>
+  );
+}
