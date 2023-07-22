a5ea8ab9af9cc9b2e31ad677baf49bdf2f9c39b6
Weslley Nascimento <weslley_sjn@hotmail.com>
2023-07-22 18:31:17 -0300

    criando e estilizando component Footer

--- frontend/todo-mobile/src/views/Home/index.js
+++ frontend/todo-mobile/src/views/Home/index.js
@@ -5,11 +5,13 @@ import styles from './styles';
 
 // COMPONENTS 
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Home() {
     return (
         <View style={styles.container}>
             <Header showNotification={true} showBack={false}></Header>
+            <Footer icon={'add'}></Footer>
         </View>
     );
 }
