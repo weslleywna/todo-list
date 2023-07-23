b584de16d8cfd02747e204b24a62038b8325337d
Weslley Nascimento <weslley_sjn@hotmail.com>
2023-07-23 15:20:13 -0300

    Criando filtros das tasks

--- frontend/todo-mobile/src/views/Home/index.js
+++ frontend/todo-mobile/src/views/Home/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
@@ -8,9 +8,45 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 export default function Home() {
+    const [filter, setFilter] = useState('today');
+
+
     return (
         <View style={styles.container}>
             <Header showNotification={true} showBack={false}></Header>
+
+            <View style={styles.filter}>
+                <TouchableOpacity onPress={() => setFilter('all')}>
+                    <Text style={filter == 'all' ? styles.filterTextActived : styles.filterTextInative}>
+                        Todos
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => setFilter('today')}>
+                    <Text style={filter == 'today' ? styles.filterTextActived : styles.filterTextInative}>
+                        Hoje
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => setFilter('month')}>
+                    <Text style={filter == 'month' ? styles.filterTextActived : styles.filterTextInative}>
+                        Mês
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => setFilter('week')}>
+                    <Text style={filter == 'week' ? styles.filterTextActived : styles.filterTextInative}>
+                        Semana
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => setFilter('year')}>
+                    <Text style={filter == 'year' ? styles.filterTextActived : styles.filterTextInative}>
+                        Ano
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
             <Footer icon={'add'}></Footer>
         </View>
     );
